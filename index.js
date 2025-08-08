4e4a4d0148fe551e501e23c703c9a0c7bd337399
sootopia <shpark7502@naver.com>
2025-08-08 22:26:06 +0900

    feat: CORS 설정 추가

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const cors = require('cors');
 const sequelize = require('./src/config/database');
 const userRoutes = require('./src/routes/userRoutes');
 const postRoutes = require('./src/routes/postRoutes');
 require('dotenv').config();
 
 const app = express();
+
+// CORS 설정
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }),
+);
+
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
